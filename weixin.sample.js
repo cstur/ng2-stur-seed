633b7938ca126ac158d67b88fea8e5e1b86e7562
stur <sdongjie@126.com>
2016-02-28 21:27:08 +0800

    xx

--- weixin.sample.js
+++ weixin.sample.js
@@ -2,6 +2,7 @@ var http   = require('http'),
     wechat = require('node-wechat')('stur');
 
 http.createServer(function (req, res) {
+
   //检验 token
   wechat.checkSignature(req, res);
   //预处理
@@ -9,26 +10,7 @@ http.createServer(function (req, res) {
 
   //监听文本信息
   wechat.text(function (data) {
-
-    //console.log(data.ToUserName);
-    //console.log(data.FromUserName);
-    //console.log(data.CreateTime);
-    //console.log(data.MsgType);
-    //...
-
-    var msg = {
-      FromUserName : data.ToUserName,
-      ToUserName : data.FromUserName,
-      //MsgType : "music",
-      Title : "宋冬野",
-      Description : "宋冬野——摩登天空7",
-      MusicUrl : "http://zhangmenshiting.baidu.com/data2/music/71272862/44897031226800128.mp3?xcode=8c25fcb0e8157c1d4ee014e7c541cba8c3b34145ef4199ad",
-      //HQMusicUrl : "http://zhangmenshiting.baidu.com/data2/music/71272862/44897031226800128.mp3?xcode=8c25fcb0e8157c1d4ee014e7c541cba8c3b34145ef4199ad",
-      //FuncFlag : 0
-    }
-
-    //回复信息
-    wechat.send(msg);
+    wechat.send('xx');
   });
 
   //监听图片信息
